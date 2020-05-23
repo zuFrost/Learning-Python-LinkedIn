af515a0849021067268238199c7bc057bebfbee4
Alex Frost <zufrost.a@gmail.com>
2020-05-24 02:08:12 +0300

    number of digits

---
+++ Ex_Files_Learning_Python/Exercise Files/Ch2/number_of_digits.py
@@ -0,0 +1,43 @@
+# Which code snippet can you use to print the number of digits in the number variable? 
+# You can assume this number is always positive and always less than 10,000.
+
+number = 9999
+
+# if (number>=0):
+#   print(1)
+# elif (number>=10):
+#   print(2)
+# elif (number>=100):
+#   print(3)
+# else:
+#   print(4)
+
+
+# if (number<=10):
+#   print(1)
+# elif (number<=100):
+#   print(2)
+# elif (number<=1000):
+#   print(3)
+# else:
+#   print(4)
+
+
+if (number>=1000):
+  print(4)
+elif (number>=100):
+  print(3)
+elif (number>=10):
+  print(2)
+else:
+  print(1)
+
+
+# if (number<10000):
+#   print(4)
+# elif (number<1000):
+#   print(3)
+# elif (number<10):
+#   print(2)
+# else:
+#   print(1)
