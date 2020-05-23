708bfa92188b8f8e39767d7bfbd1292d41d86153
Alex Frost <zufrost.a@gmail.com>
2020-05-24 02:49:08 +0300

    Python formatting time and date

--- Ex_Files_Learning_Python/Exercise Files/Ch3/formatting_start.py
+++ Ex_Files_Learning_Python/Exercise Files/Ch3/formatting_start.py
@@ -7,18 +7,26 @@ from datetime import datetime
 def main():
   # Times and dates can be formatted using a set of predefined string
   # control codes 
+  now = datetime.now()
 
   #### Date Formatting ####
   
   # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
+  # print(now.strftime("The current year is: %Y"))
+  # print(now.strftime("%a, %d %B, %y"))
 
 
   # %c - locale's date and time, %x - locale's date, %X - locale's time
+  # print(now.strftime("Local date and time: %c"))
+  # print(now.strftime("Local date: %x"))
+  # print(now.strftime("Local time: %X"))
 
 
   #### Time Formatting ####
   
   # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
+  print(now.strftime("Current time: %I:%M:%S %p"))
+  print(now.strftime("Current 24-hour time: %H:%M:%S"))
 
 
 
