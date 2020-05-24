f6daf8438caeffd36024769b6c5ae94dc2061a0a
Alex Frost <zufrost.a@gmail.com>
2020-05-24 15:04:03 +0300

    parsing and processing HTML

--- Ex_Files_Learning_Python/Exercise Files/Ch5/htmlparsing_start.py
+++ Ex_Files_Learning_Python/Exercise Files/Ch5/htmlparsing_start.py
@@ -1,11 +1,23 @@
 # 
 # Example file for parsing and processing HTML
 #
+from html.parser import HTMLParser
+
+
+class MyHTMLParser(HTMLParser):
+  def handle_comments(self, data):
+    print("Encountered comment: ", data)
+    pos = self.getpos()
+    print ("\tAt line: ", pos[0], " position ", pos[1])
 
 
 def main():
   # instantiate the parser and feed it some HTML
   parser = MyHTMLParser()
+  f = open("samplehtml.html")
+  if f.mode == 'r':
+    contents = f.read()
+    parser.feed(contents)
     
 
 if __name__ == "__main__":
