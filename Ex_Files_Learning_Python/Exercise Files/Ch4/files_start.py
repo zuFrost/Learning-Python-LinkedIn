7c4b0a9bd1d8a490fabfcd83d877aa6fb623e3c6
Alex Frost <zufrost.a@gmail.com>
2020-05-24 10:48:06 +0300

    Read and write files using the built-in Python file methods

--- Ex_Files_Learning_Python/Exercise Files/Ch4/files_start.py
+++ Ex_Files_Learning_Python/Exercise Files/Ch4/files_start.py
@@ -4,18 +4,31 @@
 
 def main():  
   # Open a file for writing and create it if it doesn't exist
-
+  # f = open("textfile.txt", "w+")
 
   # Open the file for appending text to the end
-
+  # f = open("textfile.txt", "a")
+  f = open("textfile.txt", "r")
 
   # write some lines of data to the file
+  # for i in range (10):
+  #   f.write("This is line " + str(i) + "\r\n")
 
   
   # close the file when done
+  # f.close()
 
   
   # Open the file back up and read the contents
+  if f.mode == 'r':
+    # contens = f.read()
+    fl = f.readlines()
+    for x in fl:
+      print(x)
+    # print(contens)
+  f.close
+
+
 
     
 if __name__ == "__main__":
